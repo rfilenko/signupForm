53d001216c0b9fa07a965a395036940b50b26027
Olia Starostina <roman.filenko@gmail.com>
2020-01-10 11:16:51 +0100

    small cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import FormMain from "./components/FormMain";
 import FormSignup from "./components/FormSignup";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+
+import useForm from "./useForm";
 
 import Nav from "./Nav";
 import { FormWrap } from "../styles/Wrap";
 import { FormSign } from "../styles/Form";
-import useForm from "./useForm";
 
 function FormSignup() {
   const { handleChange, handleSubmit, handleFocus, values } = useForm(submit);
