b07efd194bdee2196c16f1856992a9495e7faf2b
Olia Starostina <roman.filenko@gmail.com>
2020-01-16 14:46:05 +0100

    added firebase backend support

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBPQHxnGrHkBQufkIa0dgt5cGMGOAM_tXY",
+  authDomain: "signupform-a6b68.firebaseapp.com",
+  databaseURL: "https://signupform-a6b68.firebaseio.com",
+  projectId: "signupform-a6b68",
+  storageBucket: "signupform-a6b68.appspot.com",
+  messagingSenderId: "111101596354",
+  appId: "1:111101596354:web:7a888824539fedf177215b",
+  measurementId: "G-7GNQCDPZBM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
